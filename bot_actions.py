b58f111f294647feead0cc374d7a29fd65197aec
Egor <bronnikov.40@mail.ru>
2020-05-04 16:54:33 +0300

    VK bot

--- bot_actions.py
+++ bot_actions.py
@@ -1,21 +1,24 @@
 # Модули для работы с сообщениями ВК
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import requests
 import random
 import json
+import datetime
 
 # Метод для отправки сообщения
-def write_message(vk, user_id, message):
+def write_message(vk, user_id, message, keyboard):
     '''
     write_message(vk, user_id, message) - отправка текстового сообщения с текстом message.
     '''
     vk.method('messages.send', {
     'user_id': user_id,
     'message': message,
-    'random_id': random.getrandbits(31) * random.choice([-1, 1])
+    'random_id': random.getrandbits(31) * random.choice([-1, 1]),
+    'keyboard': keyboard
     })
 
 # Метод для отправки сообщения с картинкой
-def send_photo(vk, user_id, message, picture):
+def send_photo(vk, user_id, message, picture, keyboard):
     '''
     send_photo(vk, user_id, message, picture) - отправка сообщения сообщения с текстом message и с картинкой picture.
     '''
@@ -28,7 +31,8 @@ def send_photo(vk, user_id, message, picture):
     'user_id': user_id,
     'message': message,
     'attachment': dataPic,
-    'random_id': random.getrandbits(31) * random.choice([-1, 1])
+    'random_id': random.getrandbits(31) * random.choice([-1, 1]),
+    'keyboard': keyboard
      })
 
 # Метод для работы с уровнем пользовалеля
@@ -75,43 +79,148 @@ def link_button(link, label):
         }
     }
 
+def week(data):
+    l=[]
+    for b in range(1,8):
+        a = datetime.date.today()
+        bb = datetime.timedelta(days=b)
+        cc = a+bb
+        l.append(str(cc))
+    l1=[i[5:7] for i in l]
+    l2=[i[-2:] for i in l]
+    dr=[i for i in data if i[-3:-1] in l1 and i[-6:-4] in l1]
+    if dr==[]:
+        return 'На этой неделе пока никто не родился, но есть шанс это исправить)\nСвободен сегодня вечером?'
+    else:
+        return ''.join(dr)
+
+def send_films(list):
+    return 'Другие Primats рекомендуют:\n\n'+''.join(random.sample(list,3))
+
+def send_task(list):
+    return 'Развлекайся\n\n'+random.choice(list)
+
+def dr(m, data):
+    mon={"январь":'01','февраль':'02','март':"03",'апрель':'04','май':'05','июнь':'06','июль':'07','август':'08','сентябрь':'09',"октябрь":'10','ноябрь':'11','декабрь':'12'}
+    L=[i for i in data if i[-3:-1]==mon.get(m)]
+    if L==[]:
+        return 'В этом месяце пока никто не родился, но есть шанс это исправить)\nСвободен сегодня вечером?'
+    else:
+        return ''.join(L)
+
 # Клавиатура
-keyboard = {
-    "one_time": False,
-    "buttons": [
-        [
-            text_button("Привет", "primary"),
-            text_button("Пока","primary")
-        ],
-        [
-            text_button("Уровень", "primary"),
-            text_button("Команды","default"),
-            text_button("Почта","primary")
-        ],
-        [
-            text_button("Расписание","positive"),
-            text_button("Дедлайны","negative")
-        ],
-        [
-            link_button('http://www.rating.unecon.ru/', "БРС"),
-            link_button('https://us02web.zoom.us/j/624529129?pwd=bHBoODh3YTlad3VlNHFOcStNM3ZxQT09', "Мат. анализ")
-        ]
-    ]
-}
+def create_keyboard(response):
+    keyboard = VkKeyboard(one_time=False)
 
-# Очищаем все кодировки, так требует ВК
-keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
-keyboard = str(keyboard.decode('utf-8'))
+    if response=='кнопка на случай, если устал учиться':
+        keyboard.add_button('но не устал от математики', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_button('Что посмотреть?', color=VkKeyboardColor.POSITIVE)
+
+    elif response=='дни рождения':
+        keyboard.add_button('На месяц',color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('На неделю',color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('Все',color=VkKeyboardColor.POSITIVE)
+
+    elif response=='учебники':
+        keyboard.add_button('Мат.анализ',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Алгебра',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Дискр.мат',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Wolfram Math',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_openlink_button('Python','https://vk.com/away.php?to=https%3A%2F%2Fvk.cc%2FalOWY8&cc_key=')
+        keyboard.add_line()
+        keyboard.add_button('Английский',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='английский':
+        keyboard.add_openlink_button('Market Leader','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1xEkwAyIT2AtAuJ56xVyfFQ5w8czPtJAa%2Fview&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Facilitator','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F17P1MYWDJDREdfGQRGD9inyREZAqEXlR6%2Fview&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Practical Grammar Course','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1LJLLploS_8epi8yYWsVjm5JsDi7nmHYu%2Fview&cc_key=')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='wolfram math':
+        keyboard.add_openlink_button('Фридман,Леора','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Fopen%3Fid%3D1xg9g1LAuw-jIH6HwH9e-UE_GU9bq8Kri&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('S.Wolfram "Wolfram Language"','https://vk.com/away.php?to=https%3A%2F%2Fwww.wolfram.com%2Flanguage%2Felementary-introduction%2F2nd-ed%2Findex.html&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Help','https://reference.wolfram.com/language/')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='мат.анализ':
+        keyboard.add_openlink_button('Демидович (задачник)','https://drive.google.com/file/d/1VnHhcsfAlVg48nsYXWdXfNZiVkakamv-/view')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Виноградов, Громов','https://vk.com/doc108898977_514996947?hash=984e14ef93de90b23e&dl=a91def24fdbf7a14b9')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='алгебра':
+        keyboard.add_openlink_button('Икрамов (задачник)','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Fopen%3Fid%3D1KxIrUV3yGT_IKzz2X52JW6x1mUmoqP7C&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Воеводин','https://vk.com/away.php?to=https%3A%2F%2Fdrive.google.com%2Fopen%3Fid%3D1Cd-Q2GMVhLERwJSh1lyriE89DsBRUc6V&cc_key=')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Курош','https://vk.com/doc61070830_524793039?hash=12ae30e78883526a84&dl=afb96283e5d1fe460f')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Фадеев','https://vk.com/doc437171420_515200818?hash=336121839fac36e083&dl=dfed8ac668a6205674')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='дискр.мат':
+        keyboard.add_openlink_button('Иванов, Фридман','https://vk.com/doc234167654_514661330?hash=9fead45b2c2f3e2ce2&dl=0bef1a3cd3e9273e36')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Корте "Комбинаторная оптимизация"','https://drive.google.com/file/d/1kOZcitXuRBTKX0pgpXC8FEPenW4rjz6y/view')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='полезные ссылки':
+        keyboard.add_openlink_button('Мат. анализ','https://us02web.zoom.us/j/624529129?pwd=bHBoODh3YTlad3VlNHFOcStNM3ZxQT09')
+        keyboard.add_line()
+        keyboard.add_openlink_button('БРС','http://www.rating.unecon.ru/')
+        keyboard.add_line()
+        keyboard.add_openlink_button('Moodle','https://student.unecon.ru/')
+        keyboard.add_line()
+        keyboard.add_button('Назад',color=VkKeyboardColor.NEGATIVE)
+
+    elif response=='на месяц':
+        keyboard.add_button('Январь',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Февраль',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Март',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Апрель',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Май',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Июнь',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Июль',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Август',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Сентябрь',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Октябрь',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Ноябрь',color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Декабрь',color=VkKeyboardColor.PRIMARY)
+
+    else:
+        keyboard.add_button('Привет', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Пока', color=VkKeyboardColor.DEFAULT)
+        keyboard.add_button('Уровень', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+
+        keyboard.add_button('Расписание', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('Учебники', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Дедлайны', color=VkKeyboardColor.NEGATIVE)
+        keyboard.add_line()
+
+        keyboard.add_button('Дни Рождения', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Почта', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Полезные ссылки', color=VkKeyboardColor.DEFAULT)
+        keyboard.add_line()
+
+        keyboard.add_button('Кнопка на случай, если устал учиться', color=VkKeyboardColor.PRIMARY)
 
 
-# Метод для предоставления пользовалелю клавиатуры
-def vk_keyboard(vk, user_id, keyboard):
-    '''
-    vk_keyboard(vk, user_id, keyboard) - выдаёт пользователю клавиатуру.
-    '''
-    vk.method("messages.send", {
-                "user_id": user_id,
-                "message": "⌨ Используй клавиатуру для общения со мной",
-                "random_id": random.getrandbits(31) * random.choice([-1, 1]),
-                "keyboard": keyboard
-                })
+    keyboard = keyboard.get_keyboard()
+    return keyboard
--- vk_bot.py
+++ vk_bot.py
@@ -7,7 +7,6 @@ from bot_actions import * # Модуль для работы с ботом, ба
 from deadlines_data import * # Модуль для работы с таблицей дедлайнов
 
 # Доп. модули
-import datetime
 import os
 from funcy import group_by
 from bs4 import BeautifulSoup
@@ -28,7 +27,7 @@ for hello in greetings:
 goodbye_list = ['Пока-пока, человек...', 'Пока!\nБудь здоров!', 'Увидимся!', 'Пока, бывай', 'До скорого']
 
 # Токен нашей группы и работа с запросами
-_TOKEN = 'токен группы'
+_TOKEN = '3d423eb8812629fc6834d96bd0b5352f75f83f7691f828ca84ac57b909bf2ff519f438bc6aa4d9316cc03'
 vk = vk_api.VkApi(token=_TOKEN)
 longpoll = VkLongPoll(vk)
 
@@ -62,43 +61,86 @@ with open('data/oddWeek.txt', 'r', encoding="utf-8") as file_odd, open('data/eve
     oddWeek = file_odd.read()
     evenWeek = file_even.read()
 
-# Определяет текущую неделю, мы выявили опытным путём, что начало недель датируется 11.8.2019
-weekNumber = Week.fromdate(datetime.date(2019,8,11)).weektuple()[-1]
+# Дни рождения
+with open('data/birth.txt','r', encoding='utf-8') as birthdays:
+    birthdaysList=birthdays.readlines()
+
+# Фильмы
+with open ('data/films.txt','r', encoding='utf-8') as films:
+    filmlist=films.readlines()
+
+# Олипипиадные задачки
+with open('data/olimp.txt','r', encoding='utf-8') as olimp:
+    olimplist=olimp.readlines()
 
 
 # Запускаем нашего бота
 for session in longpoll.listen():
     if session.type == VkEventType.MESSAGE_NEW:
+        user_message = session.text.lower()
+        keyboard = create_keyboard(user_message)
         if session.to_me:
-            user_message = session.text
+            if user_message == commands_list[0]: # привет
+                send_photo(vk, session.user_id, random.choice(_EMOJIS) + random.choice(greetings_list[25:-40]), random.choice(_PICTURES['hello']), keyboard)
 
-            if user_message.lower() == commands_list[0]: # привет
-                send_photo(vk, session.user_id, random.choice(_EMOJIS) + random.choice(greetings_list[25:-40]), random.choice(_PICTURES['hello']))
+            elif user_message == commands_list[1]: # пока
+                send_photo(vk, session.user_id, random.choice(_EMOJIS) + random.choice(goodbye_list),random.choice(_PICTURES['bye']), keyboard)
 
-            elif user_message.lower() == commands_list[1]: # пока
-                send_photo(vk, session.user_id, random.choice(_EMOJIS) + random.choice(goodbye_list),random.choice(_PICTURES['bye']))
-
-            elif user_message.lower() == commands_list[2]: # расписание
+            elif user_message == commands_list[2]: # расписание
+                # Определяет текущую неделю, мы выявили опытным путём, что начало недель датируется 10.8.2019
+                weekNumber = Week.fromdate(datetime.date(2019,8,10)).weektuple()[-1]
                 if weekNumber%2 == 0:
-                    write_message(vk, session.user_id, evenWeek)
+                    write_message(vk, session.user_id, evenWeek, keyboard)
                 else:
-                    write_message(vk, session.user_id, oddWeek)
+                    write_message(vk, session.user_id, oddWeek, keyboard)
 
-            elif user_message.lower() == commands_list[3]: # команды
-                write_message(vk, session.user_id, messages_list[3])
+            elif user_message == commands_list[3]: # команды
+                write_message(vk, session.user_id, messages_list[3], keyboard)
 
-            elif user_message.lower() == commands_list[4]: # дедланы
-                write_message(vk, session.user_id, update_deadlines(client))
+            elif user_message == commands_list[4]: # дедланы
+                write_message(vk, session.user_id, update_deadlines(client), keyboard)
 
-            elif user_message.lower() == commands_list[5]: # почта
-                write_message(vk, session.user_id, messages_list[5])
+            elif user_message == commands_list[5]: # почта
+                write_message(vk, session.user_id, messages_list[5], keyboard)
 
-            elif user_message.lower() == commands_list[6]: # добавляем кол-во сообщений в переписке
-                send_photo(vk, session.user_id, history_messages(vk, session.user_id), _PICTURES['level'][0])
+            elif user_message == commands_list[6]: # добавляем кол-во сообщений в переписке
+                send_photo(vk, session.user_id, history_messages(vk, session.user_id), _PICTURES['level'][0], keyboard)
 
+            elif user_message=='дни рождения':
+                write_message(vk,session.user_id,'Выбери нужный вариант',keyboard=keyboard)
 
-            elif user_message.lower() == commands_list[7] or user_message.lower() == commands_list[8]: # обновляем клавиатуру
-                vk_keyboard(vk, session.user_id, keyboard)
+            elif user_message=='на месяц':
+                write_message(vk,session.user_id,'Выбери нужный месяц',keyboard=keyboard)
+
+            elif user_message=='на неделю':
+                write_message(vk,session.user_id,week(birthdaysList),keyboard=keyboard)
+
+            elif user_message=='все':
+                write_message(vk,session.user_id,''.join(birthdaysList),keyboard=keyboard)
+
+            elif user_message=='январь' or user_message=='февраль' or user_message=='март' or user_message=='апрель' or user_message=='май' or user_message=='июнь' or user_message=='июль' or user_message=='август' or user_message=='сентябрь' or user_message=='октябрь' or user_message=='ноябрь' or user_message=='декабрь':
+                write_message(vk,session.user_id, dr(user_message, birthdaysList), keyboard=keyboard)
+
+            elif user_message=='учебники':
+                write_message(vk,session.user_id,  'Выбери нужный предмет',keyboard=keyboard)
+
+            elif user_message=='английский' or user_message=='алгебра' or user_message=='wolfram math' or user_message=='мат.анализ' or user_message=='дискр.мат':
+                write_message(vk,session.user_id,  'Выбери нужный учебник',keyboard=keyboard)
+
+            elif user_message=='назад':
+                write_message(vk,session.user_id ,'Вываливаемся товарищи',keyboard=keyboard)
+
+            elif user_message=='кнопка на случай, если устал учиться':
+                write_message(vk,session.user_id ,'Как же я тебя понимаю...',keyboard=keyboard)
+
+            elif user_message=='что посмотреть?':
+                write_message(vk,session.user_id ,send_films(filmlist),keyboard=keyboard)
+
+            elif user_message=='но не устал от математики':
+                write_message(vk,session.user_id ,send_task(olimplist),keyboard=keyboard)
+
+            elif user_message=='полезные ссылки':
+                write_message(vk,session.user_id ,'Ммммм',keyboard=keyboard)
 
             else:
-                write_message(vk, session.user_id, "Я тупой бот и не понимаю вашего человеческого языка...\n\n" + messages_list[3])
+                write_message(vk, session.user_id, "Я тупой бот и не понимаю вашего человеческого языка...\n\n" + messages_list[3],keyboard)
